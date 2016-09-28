608c891dd633dfdf1cb8ee8574a64b9bfbf0dbed
Jorge Fco <jorge_frac@hotmail.com>
2016-09-28 11:30:17 -0500

    Init project

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<!-- Control the behavior of search engine crawling and indexing -->
+	<meta name="robots" content="index,follow,noodp"><!-- All Search Engines -->
+	<meta name="googlebot" content="index,follow"><!-- Google Specific -->
+	<!-- Does the same function as the keywords tag -->
+	<meta name="category" content="">
+
+	<!--FAVICONS-->
+	<link rel="apple-touch-icon" sizes="57x57" href="images/favicons/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="images/favicons/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="images/favicons/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="images/favicons/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="images/favicons/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="images/favicons/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="images/favicons/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="images/favicons/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="images/favicons/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="images/favicons/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="images/favicons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="images/favicons/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="images/favicons/favicon-16x16.png">
+	<link rel="manifest" href="images/favicons/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
+
+
+	<title>:: Page ::</title>
+
+	<!--CSS-->
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+
+	<!--HEADER-->
+	<header>
+		<div class="container">
+			<nav>
+				<ul class="nav">
+					<li><a href="" title=""></a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+	<!--HEADER-->
+
+	<!--SECTION-->
+	<div class="home">
+		<div class="container">
+			<section>
+				<h1>Home</h1>
+				<p>Text</p>
+				<button type="button" class="btn btn-default round">Go!</button>
+			</section>
+		</div>
+	</div>
+	<!--SECTION-->
+
+	<!--FOOTER-->
+	<footer>
+		<div class="container">
+			<p> <?php echo date('Y') ?></p>
+		</div>
+	</footer>
+	<!--FOOTER-->
+
+	<!--JS-->
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.8/angular.min.js"></script>
+	<script src="js/jquery.min.js" type="text/javascript"></script>
+	<script src="js/plugins.js" type="text/javascript"></script>
+	<script src="js/main.js" type="text/javascript"></script>
+
+	<!-- Google Analytics -->
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-XXXXX-Y', 'auto');
+		ga('send', 'pageview');
+	</script>
+	<!-- End Google Analytics -->
+
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,24 @@
+/*!
+	* main.js 	|	Jorge Fco.
+	* Copyright 2016
+*/
+(function($){
+	//Var
+
+	//Init
+	main = {
+		init: function(){
+			main.Home();
+		},
+
+		//Functions
+		Home: function(){
+			//Code
+		},
+
+	};//end main
+
+	//Run
+	main.init();
+
+})(jQuery);

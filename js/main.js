2d450e6c8976ebb67cff740eb73e8b88fe0d0684
Jorge Fco <jorge_frac@hotmail.com>
2016-10-23 11:23:50 -0500

    Add colors section

--- js/main.js
+++ js/main.js
@@ -9,9 +9,6 @@
 	//Init
 	main = {
 		init: function(){
-			//Init Home
-			TweenMax.to($('.text--1'),0.5,{autoAlpha:1,x:0,delay: 0.5, ease:Expo.easeInOut});
-			TweenMax.to($('.img--1'),0.5,{autoAlpha:1, x:0, delay: 0.5, ease:Expo.easeInOut});
 			main.Home();
 		},
 
@@ -19,7 +16,7 @@
 		Home: function(){
 			FullPage.fullpage({
 				navigation: true,
-				scrollingSpeed: 1500,
+				scrollingSpeed: 1800,
 				fitToSectionDelay: 1000,
 				onLeave: function(index, newIndex, direction){
 					if (newIndex==1) {
@@ -66,11 +63,6 @@
 						TweenMax.to($('.text--3'),0.5,{autoAlpha:0,x:500,delay: 0.5, ease:Expo.easeInOut});
 						TweenMax.to($('.img--3'),0.3,{autoAlpha:0, x:-500, delay: 0.3, ease:Expo.easeIn});
 					}
-				},
-				afterLoad: function(anchorLink, index) {
-					if (index==1) {
-						
-					}
 				}
 			});
 		},
@@ -81,10 +73,23 @@
 	window.main = main;
 	$(window).load(function() {
 		TweenMax.to($(".text--name"),0.5,{opacity:1,delay:0.5,y:0,ease:Expo.easeInOut});
-		TweenMax.to($(".text--intro"),0.5,{width:'100%',delay:1,ease:Expo.easeInOut});
-		TweenMax.to($('.line--1'),0.7,{width:0,delay:2,ease:Expo.easeInOut});
-		TweenMax.to($('.line--2'),0.7,{x:1920,delay:2,ease:Expo.easeInOut});
-		TweenMax.to($('#loader'), 0.5,{autoAlpha:0, display:'none',ease:Power3.easeInOut, delay:3});
+		TweenMax.to($(".text--intro"),0.5,{width:'100%',delay:1,ease:Expo.easeInOut,onComplete:bgText});
+		function bgText(){
+			TweenMax.to($(".text--intro"),0.5,{height:'0',delay:0.5,ease:Expo.easeInOut,onComplete:endColor});
+			TweenMax.to($(".text--name"),0.7,{y:500,opacity:0,delay:0.7,ease:Expo.easeInOut});
+		}
+		function endColor(){
+			TweenMax.to($('.line--1'),0.5,{width:0,delay:0.7,ease:Expo.easeInOut});
+			TweenMax.to($('.line--2'),0.5,{x:1920,delay:0.7,ease:Expo.easeInOut,onComplete:endLoader});
+		}
+		function endLoader() {
+			TweenMax.to($('#loader'), 0.5,{autoAlpha:0, display:'none',ease:Power3.easeInOut, delay:0.5,onComplete:introHome});
+		}
+		function introHome(){
+			//Init Home
+			TweenMax.to($('.text--1'),0.3,{autoAlpha:1,x:0,delay: 0.2, ease:Expo.easeInOut});
+			TweenMax.to($('.img--1'),0.3,{autoAlpha:1, x:0, delay: 0.2, ease:Expo.easeInOut});
+		}
 		main.init();
 	});
 
